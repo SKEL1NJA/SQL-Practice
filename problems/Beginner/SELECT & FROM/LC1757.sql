fbb71e084d498808411015c2d948dd079eb25d86
SKEL1NJA <khannauday55@gmail.com>
2026-05-08 21:10:47 +0530

    SQL Beginner: SELECT & FROM: Leetcode problems 1757, 584, 595 solved and accepted

---
+++ problems/Beginner/SELECT & FROM/LC1757.sql
@@ -0,0 +1,19 @@
+USE problems;
+
+Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'));
+Truncate table Products;
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N');
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N');
+
+DESCRIBE products;
+SELECT * FROM products;
+
+
+-- ANSWER: https://leetcode.com/problems/recyclable-and-low-fat-products/submissions/1998215809/
+
+SELECT product_id FROM products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
+
---
+++ problems/Beginner/SELECT & FROM/LC584.sql
@@ -0,0 +1,18 @@
+use problems;
+
+Create table If Not Exists Customer (id int, name varchar(25), referee_id int);
+Truncate table Customer;
+insert into Customer (id, name, referee_id) values ('1', 'Will', NULL);
+insert into Customer (id, name, referee_id) values ('2', 'Jane', NULL);
+insert into Customer (id, name, referee_id) values ('3', 'Alex', '2');
+insert into Customer (id, name, referee_id) values ('4', 'Bill', NULL);
+insert into Customer (id, name, referee_id) values ('5', 'Zack', '1');
+insert into Customer (id, name, referee_id) values ('6', 'Mark', '2');
+
+DESCRIBE customer;
+SELECT * FROM customer;
+
+-- ANSWER: https://leetcode.com/problems/find-customer-referee/submissions/1998224424/
+
+SELECT name FROM customer
+WHERE referee_id <> 2 OR referee_id IS NULL;
